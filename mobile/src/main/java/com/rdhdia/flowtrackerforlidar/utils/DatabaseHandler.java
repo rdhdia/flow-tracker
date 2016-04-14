52df081a85e1c457f811c24199db4828616213eb
rdhdia <ruffy.heredia@gmail.com>
2016-04-14 09:20:38 +0800

    Added DatabaseHandler class

---
+++ mobile/src/main/java/com/rdhdia/flowtrackerforlidar/utils/DatabaseHandler.java
@@ -0,0 +1,9 @@
+package com.rdhdia.flowtrackerforlidar.utils;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by ruffyheredia on 14/04/2016.
+ */
+public class DatabaseHandler extends SQLiteDatabase {
+}

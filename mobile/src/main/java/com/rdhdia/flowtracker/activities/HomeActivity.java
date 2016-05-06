9ea07c1fdc4580ccf6f45685f0f91d6a34a04281
rdhdia <ruffy.heredia@gmail.com>
2016-05-06 14:28:35 +0800

    Show list of sessions on HomeActivity

--- mobile/src/main/java/com/rdhdia/flowtracker/activities/HomeActivity.java
+++ mobile/src/main/java/com/rdhdia/flowtracker/activities/HomeActivity.java
@@ -8,23 +8,36 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.rdhdia.flowtracker.BuildConfig;
 import com.rdhdia.flowtracker.R;
+import com.rdhdia.flowtracker.models.Session;
+import com.rdhdia.flowtracker.views.adapters.SessionAdapter;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+import io.realm.RealmResults;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Bind(R.id.btnStartNewSession) FloatingActionButton newSession;
     @Bind(R.id.recyclerSessions) RecyclerView recyclerSessions;
 
+    Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+
+        // Create a RealmConfiguration which is to locate Realm file in package's "files" directory.
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(HomeActivity.this).build();
+        // Get a Realm instance for this thread
+        realm = Realm.getInstance(realmConfig);
 
         recyclerSessions.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(HomeActivity.this,
@@ -37,6 +50,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         newSession.setOnClickListener(new NewSessionListener());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        RealmResults<Session> sessions = realm.where(Session.class).findAll();
+
+        if ( sessions.size() > 0 ) {
+            SessionAdapter adapter = new SessionAdapter(sessions, HomeActivity.this);
+            recyclerSessions.setAdapter(adapter);
+        } else {
+            Toast.makeText(HomeActivity.this, "No sessions found", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     private class NewSessionListener implements View.OnClickListener {
---
+++ mobile/src/main/java/com/rdhdia/flowtracker/views/adapters/SessionAdapter.java
@@ -0,0 +1,60 @@
+package com.rdhdia.flowtracker.views.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.rdhdia.flowtracker.R;
+import com.rdhdia.flowtracker.models.Session;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.realm.RealmResults;
+
+/**
+ * Created by rdhdia on 06/05/2016.
+ */
+public class SessionAdapter  extends RecyclerView.Adapter<SessionAdapter.ViewHolder> {
+
+    private RealmResults<Session> items;
+    private Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.lblSessionId)
+        TextView sessionId;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+    }
+
+    public SessionAdapter(RealmResults items, Context context) {
+        this.items = items;
+        this.context = context;
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.items.size();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_session, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Session session = items.get(position);
+
+        holder.sessionId.setText(session.getId());
+    }
+}
